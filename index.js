379fab76443d1958851d7386416fe25726f05923
Martin Marinov <martin.marinov10@yahoo.com>
2025-05-18 00:54:28 +0100

    Update the textContent depending on the result of the game

--- index.js
+++ index.js
@@ -27,4 +27,7 @@ function playGame(playerChoice) {
         }
     }
     
+    playerDisplay.textContent = `PLAYER: ${playerChoice}`;
+    computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
+    resultDisplay.textContent = result;
 }
