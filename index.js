728bf629c51b81093c2d7897377141481c3d3bb9
Martin Marinov <martin.marinov10@yahoo.com>
2025-05-18 00:52:35 +0100

    Made the functionality of the game itself

--- index.js
+++ index.js
@@ -6,6 +6,25 @@ const resultDisplay = document.getElementById('resultDisplay');
 function playGame(playerChoice) {
     const computerChoice = choices[Math.floor(Math.random() * 3)];
 
-    console.log(computerChoice);
+    let result = '';
+
+    if (playerChoice === computerChoice) {
+        result = "IT'S A TIE!"
+
+    } else {
+        switch (playerChoice) {
+            case 'rock':
+                result = (computerChoice === 'scissors') ? "YOU WIN!" : "YOU LOSE!";
+                break;
+            
+            case 'paper':
+                result = (computerChoice === 'rock') ? "YOU WIN!" : "YOU LOSE!";
+                break;    
+
+            case 'scissors':
+                result = (computerChoice === 'paper') ? "YOU WIN!" : "YOU LOSE!";
+                break;
+        }
+    }
     
 }
