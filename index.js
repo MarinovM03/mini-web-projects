acd9288e0a8d2d21cdd993ecaa03cf16277a2d1c
Martin Marinov <martin.marinov10@yahoo.com>
2025-05-18 00:47:36 +0100

    Started the index.js and created the computer choice to be a random choice between rock, paper, scissors

---
+++ index.js
@@ -0,0 +1,8 @@
+const choices = ['rock', 'paper', 'scissors'];
+const playerDisplay = document.getElementById('playerDisplay');
+const computerDisplay = document.getElementById('computerDisplay');
+const resultDisplay = document.getElementById('resultDisplay');
+
+function playGame(playerChoice) {
+    const computerChoice = [Math.floor(Math.random() * 3)];
+}
