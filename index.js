7cabcc306af931ef912992b44ab49514bdc63bf0
Martin Marinov <martin.marinov10@yahoo.com>
2025-05-18 01:14:31 +0100

    Made score functionality

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const choices = ['rock', 'paper', 'scissors'];
+
 const playerDisplay = document.getElementById('playerDisplay');
 const computerDisplay = document.getElementById('computerDisplay');
 const resultDisplay = document.getElementById('resultDisplay');
+
+const playerScoreDisplay = document.getElementById('playerScoreDisplay');
+const computerScoreDisplay = document.getElementById('computerScoreDisplay');
+let playerScore = 0;
+let computerScore = 0;
 
 function playGame(playerChoice) {
     const computerChoice = choices[Math.floor(Math.random() * 3)];
@@ -36,10 +42,14 @@ function playGame(playerChoice) {
     switch(result) {
         case 'YOU WIN!':
             resultDisplay.classList.add('greenText');
+            playerScore++;
+            playerScoreDisplay.textContent = playerScore;
             break;
 
         case 'YOU LOSE!':
             resultDisplay.classList.add('redText');
+            computerScore++;
+            computerScoreDisplay.textContent = computerScore;
             break;
     }
 }
