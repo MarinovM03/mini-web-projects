a320d49f38ec605c55a0f9e800e342ce9e90ccbc
Martin Marinov <martin.marinov10@yahoo.com>
2025-06-29 00:53:46 +0100

    Add initial stopwatch JS

---
+++ Stopwatch/index.js
@@ -0,0 +1,18 @@
+const displayEl = document.getElementById('display');
+
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
+
+function start() {
+
+}
+
+function stop() {
+    
+}
+
+function reset() {
+    
+}
