88af985a65683bbb77d4b41a3d62a91cede141a5
Martin Marinov <martin.marinov10@yahoo.com>
2025-08-30 01:02:48 +0100

    Add initialization on DOMContentLoaded

--- Kanban Board/script.js
+++ Kanban Board/script.js
@@ -1,3 +1,13 @@
 let cardIdCounter = 1;
 let currentListId = null;
 
+document.addEventListener('DOMContentLoaded', function() {
+    initializeKanbanBoard();
+});
+
+function initializeKanbanBoard() {
+    setupDragAndDrop();
+    setupAddCardButtons();
+    setupModal();
+}
+
