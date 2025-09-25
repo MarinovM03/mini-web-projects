736401689d375520c7dda9efe090cc0e7186b41b
Martin Marinov <martin.marinov10@yahoo.com>
2025-09-25 22:12:58 +0100

    Add generate random color function

--- Color Palette Generator/script.js
+++ Color Palette Generator/script.js
@@ -9,5 +9,15 @@ function generatePalette() {
     for (let i = 0; i < 5; i++) {
         colors.push(generateRandomColor());
     }
+function generateRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+
+    return color;
 }
 
+generatePalette();
